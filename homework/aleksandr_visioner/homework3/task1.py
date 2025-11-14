a11748e704b4617cdb921f1f8e540ec7909b6fc8
Aleksandr Stanovoy <aleksandrstan87@gmail.com>
2025-11-14 18:34:38 +0300

    Adding tasks for homework3 folder

---
+++ homework/aleksandr_visioner/homework3/task1.py
@@ -0,0 +1,12 @@
+# Даны 2 числа a и b. Получить их сумму, разность и произведение
+
+a = float(input('Введите первое число (a): '))
+b = float(input('fefВведите первое число (b): '))
+
+sum_result = a + b
+dif_result = a - b
+multiply_result = a * b
+
+print('Сумма двух чисел: ', sum_result)
+print('Разницы двух чисел: ', dif_result)
+print('Произведение двух чисел: ', multiply_result)
