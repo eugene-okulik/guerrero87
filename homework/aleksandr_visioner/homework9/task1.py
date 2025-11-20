c83eb2ae85cd4f1989a92ab5b1e5b5b8b5bc9584
Aleksandr Stanovoy <aleksandrstan87@gmail.com>
2025-11-20 23:11:51 +0300

    fixing flake8

--- homework/aleksandr_visioner/homework9/task1.py
+++ homework/aleksandr_visioner/homework9/task1.py
@@ -6,7 +6,4 @@ python_date = datetime.strptime(date_string, "%b %d, %Y - %H:%M:%S")
 print(f"1) Дата в питоновском формате: {python_date}")
 print(f"2) Полное название месяца: {python_date.strftime('%B')}")
 print(f"3) Отформатированная дата и время: "
-      f"{python_date.strftime("%d.%m.%Y, %H:%M")}")
-
-print(f"{salary}, {bonus} - "
-      f"${salary + random.randint(int(salary * 0.1), int(salary * 0.5))}")
+      f"{python_date.strftime('%d.%m.%Y, %H:%M')}")
