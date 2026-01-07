be26aee97e273120ea14da0013006d833658958b
Aleksandr Stanovoy <aleksandrstan87@gmail.com>
2026-01-07 23:44:21 +0300

    fixing flake8

--- homework/aleksandr_visioner/homework34/main.py
+++ homework/aleksandr_visioner/homework34/main.py
@@ -5,11 +5,11 @@ import requests
 while True:
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Текущее время: {current_time}")
-    
+
     try:
         response = requests.get("https://google.com")
         print(f"Статус запроса к Google: {response.status_code}")
     except Exception as e:
         print(f"Ошибка: {e}")
-    
+
     time.sleep(2)
